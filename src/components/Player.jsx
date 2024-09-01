98954fc65923dd8b94c1e514d83f155beeb32c36
Anurag Thakur <anutha13@gmail.com>
2024-09-01 23:46:55 +0530

    added gameboard template

---
+++ src/components/Player.jsx
@@ -0,0 +1,29 @@
+import {useState} from 'react';
+
+export default function Player({ initialName, symbol}){
+    const [playerName, setPlayerName] = useState(initialName)
+    const [isEditing, setIsEditing] = useState(false)
+
+    function handleEditClick(){
+        setIsEditing((editing)=>!editing)
+    }
+
+    function handleChange(e){
+console.log(e)
+        setPlayerName(e.target.value)
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>
+
+    if(isEditing){
+        editablePlayerName = <input type='text' onChange={handleChange} required value={playerName}/>
+    }
+
+    return (<li>
+    <span className="player">
+    {editablePlayerName}
+    <span className="player-symbol">{symbol}</span>
+    </span>
+    <button onClick={handleEditClick}>{isEditing ? 'Save' : "Edit"}</button>
+  </li>)
+}
